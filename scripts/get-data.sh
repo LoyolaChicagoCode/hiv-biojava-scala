dca46b72aefb8d8c6e525df2aff7fba5d077a401
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-10-05 15:36:05 -0500

    no large files in repo
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ scripts/get-data.sh
@@ -0,0 +1,7 @@
+rm -rf data/
+mkdir -p data
+#wget http://files.figshare.com/2304759/pseudomonas.tar.gz
+wget http://files.figshare.com/2304758/hiv.tar.gz
+tar xzf hiv.tar.gz -C data/
+tar xzf pseudomonas.tar.gz -C data/
+
